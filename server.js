d86cd66656eff54a2f58ab288cac6688715627db
Loshith Hasinda Samararathna <it20127046@my.sliit.lk>
2022-06-02 14:16:00 +0530

    Netfliy

--- server.js
+++ server.js
@@ -69,32 +69,32 @@ app.get("/", function(req, res) {
 
 // Socket.io - Server configurations and functionalities
 
-// const server = http.createServer(app);
+const server = http.createServer(app);
 
-// server.listen(3001, () => {
-//   console.log("CHAT SERVER RUNNING");
-// });
+server.listen(process.env.PORT || 3001, () => {
+  console.log("CHAT SERVER RUNNING");
+});
 
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST"],
-//   },
-// });
+const io = new Server(server, {
+  cors: {
+    origin: "https://deft-genie-adc47c.netlify.app",
+    methods: ["GET", "POST"],
+  },
+});
 
-// io.on("connection", (socket) => {
-//   console.log(`User Connected: ${socket.id}`);
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
 
-//   socket.on("join_room", (data) => {
-//     socket.join(data);
-//     console.log(`User with ID: ${socket.id} joined room: ${data}`);
-//   });
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
 
-//   socket.on("send_message", (data) => {
-//     socket.to(data.room).emit("receive_message", data);
-//   });
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
 
-//   socket.on("disconnect", () => {
-//     console.log("User Disconnected", socket.id);
-//   });
-// });
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
