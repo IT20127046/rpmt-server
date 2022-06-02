0a06a2d81d246b3f08f2ec4bcc127bd28b4daa9d
Loshith Hasinda Samararathna <it20127046@my.sliit.lk>
2022-06-02 14:29:22 +0530

    Upadte server js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
 const cors = require("cors");
 
-const http = require("http");
-const { Server } = require("socket.io");
+// const http = require("http");
+// const { Server } = require("socket.io");
 
 require("dotenv").config();
 
@@ -69,32 +69,32 @@ app.get("/", function(req, res) {
 
 // Socket.io - Server configurations and functionalities
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3001, () => {
-  console.log("CHAT SERVER RUNNING");
-});
+// server.listen(process.env.PORT || 3001, () => {
+//   console.log("CHAT SERVER RUNNING");
+// });
 
-const io = new Server(server, {
-  cors: {
-    origin: "https://deft-genie-adc47c.netlify.app",
-    methods: ["GET", "POST"],
-  },
-});
+// const io = new Server(server, {
+//   cors: {
+//     origin: "https://deft-genie-adc47c.netlify.app",
+//     methods: ["GET", "POST"],
+//   },
+// });
 
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
+// io.on("connection", (socket) => {
+//   console.log(`User Connected: ${socket.id}`);
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
-  });
+//   socket.on("join_room", (data) => {
+//     socket.join(data);
+//     console.log(`User with ID: ${socket.id} joined room: ${data}`);
+//   });
 
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
+//   socket.on("send_message", (data) => {
+//     socket.to(data.room).emit("receive_message", data);
+//   });
 
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
-  });
-});
+//   socket.on("disconnect", () => {
+//     console.log("User Disconnected", socket.id);
+//   });
+// });
